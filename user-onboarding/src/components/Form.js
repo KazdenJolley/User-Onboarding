92952a676b8c2f6be310786399244072bd130959
kazden.jolley <kazden.jolley@gmail.com>
2021-05-19 09:21:07 -0600

    created form

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,52 @@
+import '../App.css';
+import React from 'react';
+
+export default (props) => {
+    const { values } = props
+
+    const onChange = () => {
+
+    }
+
+    const onSubmit = () => {
+
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <label>
+                Name: <input
+                    type='text'
+                    name='name'
+                    value={values.name}
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+                Email: <input
+                    type='email'
+                    name='email'
+                    value={values.email}
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+                Password: <input
+                    type='text'
+                    name='password'
+                    value={values.password}
+                    onChange={onChange}
+                />
+            </label>
+            <label>
+                Terms of Service: <input
+                    type='checkbox'
+                    name='terms'
+                    value={values.terms}
+                    onChange={onChange}
+                />
+            </label>
+            <button>Submit</button>
+        </form>
+    )
+}
