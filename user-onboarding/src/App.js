7f5843f691b299417d34bbbc8040c91910473dfb
kazden.jolley <kazden.jolley@gmail.com>
2021-05-19 10:39:10 -0600

    added validation

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import React, { useState } from 'react';
+import * as yup from 'yup';
 
 import Form from './components/Form';
+import schema from './validation/Schema';
+
 
 const initialValues = {
   name: '',
@@ -21,9 +24,40 @@ function App() {
   const [formValues, setFormValues] = useState(initialValues);
   const [errors, setErrors] = useState(initialErrors);
 
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(valid => {
+        setErrors({
+          ...errors,
+          [name]: ''
+        })
+      })
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [name]: err.errors[0]
+        })
+      })
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value);
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const submitForm = () => {
+
+  }
+
   return (
     <div className="App">
-      <Form values={formValues}/>
+      <Form values={formValues} errors={errors} updateValues={inputChange} submit={submitForm} />
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,51 +2,61 @@ import '../App.css';
 import React from 'react';
 
 export default function Form(props) {
-    const { values } = props
+    const { values, updateValues, submit, errors } = props
 
-    const onChange = () => {
-
+    const onChange = evt => {
+        const { name, value, checked, type } = evt.target;
+        updateValues([name], type === 'checkbox' ? checked : value)
     }
 
-    const onSubmit = () => {
-
+    const onSubmit = evt => {
+        evt.preventDefault();
+        onSubmit()
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <label>
-                Name: <input
-                    type='text'
-                    name='name'
-                    value={values.name}
-                    onChange={onChange}
-                />
-            </label>
-            <label>
-                Email: <input
-                    type='email'
-                    name='email'
-                    value={values.email}
-                    onChange={onChange}
-                />
-            </label>
-            <label>
-                Password: <input
-                    type='text'
-                    name='password'
-                    value={values.password}
-                    onChange={onChange}
-                />
-            </label>
-            <label>
-                Terms of Service: <input
-                    type='checkbox'
-                    name='terms'
-                    value={values.terms}
-                    onChange={onChange}
-                />
-            </label>
-            <button>Submit</button>
-        </form>
+        <div className='container'>
+            <form onSubmit={onSubmit}>
+                <label>
+                    Name: <input
+                        type='text'
+                        name='name'
+                        value={values.name}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>
+                    Email: <input
+                        type='email'
+                        name='email'
+                        value={values.email}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>
+                    Password: <input
+                        type='text'
+                        name='password'
+                        value={values.password}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>
+                    Terms of Service: <input
+                        type='checkbox'
+                        name='terms'
+                        value={values.terms}
+                        onChange={onChange}
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
+            <div>
+                <div>{errors.name}</div>
+                <div>{errors.email}</div>
+                <div>{errors.password}</div>
+                <div>{errors.terms}</div>
+            </div>
+        </div>
     )
 }
